cdb00e7046deb48e8616b94b6c27e1a47d4c6643
Nishant <168670710+chikuchaub@users.noreply.github.com>
2025-03-04 06:10:37 +0530

    Initial commit

---
+++ JavaScript/first.js
@@ -0,0 +1,66 @@
+// fullname = "Sulo";
+// age = 24;
+// price = 49;
+// x = null;
+// y = undefined;
+// isfollow = true;
+
+// fullname = 25;
+
+// console.log(y);
+// console.log(fullname);
+
+
+
+
+// var age = 24;
+// var age = 59;
+// var age = 86;
+
+// console.log(age);
+
+// let age = 24;
+// age = 59;
+// age = 86;
+
+// console.log(age);
+
+
+// const age = 59;
+
+// console.log(age);
+
+// {
+//     let a = 5;
+//     console.log(a); 
+// }
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// {
+//     x = BigInt("123");
+// }
+
+// {
+//     y = Symbol("Hello!");
+// }
+
+const student = {
+    fullName : "Rahul Singh",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true
+};
+
+console.log(student.cgpa);
+
+student["age"] = student["age"] + 1;
+
+console.log(student.age);
+
+student["name"] = "Rahul Kumar";
+
+console.log(student.name);
