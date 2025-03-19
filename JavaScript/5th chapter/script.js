8bfa90ee1219e2d975818c726b50ee7e419b4229
Nishant <168670710+chikuchaub@users.noreply.github.com>
2025-03-19 20:04:45 +0530

    chap 5 completed

--- JavaScript/5th chapter/script.js
+++ JavaScript/5th chapter/script.js
@@ -89,8 +89,80 @@
 //     console.log(val.toUpperCase(), idx, arr);
 // });
 
-let arr = [1, 2, 3, 4, 5];                                                  //Prectice Que
+// let arr = [1, 2, 3, 4, 5];                                                  //Prectice Que
 
-arr.forEach((val) => {
-  console.log(val * val);
+// arr.forEach((val) => {
+//   console.log(val * val);
+// });
+
+
+//  let nums = [67,52,39];                                      //map ELEMENT
+
+//  nums.map((val) =>{
+//     console.log(val);
+//  });
+
+
+//  let nums = [67,52,39];                                      //map ELEMENT
+
+//  let newArr = nums.map((val) =>{
+//     return val;
+//  });
+
+//  console.log(newArr);
+
+
+//  let Arr = [1,2,3,4,5,6,7];                                  //filter ELEMENT
+
+//  let evenArr = Arr.filter((val) =>  {
+//     return val % 2 === 0;
+//  });
+
+//  console.log(evenArr);
+
+
+// let Arr = [1, 2, 3, 4];                                     //reduce ELEMENT
+
+// const output = Arr.reduce((res, curr) => {
+//     return res + curr;
+// });
+
+// console.log(output);  //10
+
+
+// let Arr = [111, 52, 63, 14];                                     //reduce ELEMENT
+
+// const output = Arr.reduce((prev, curr) => {
+//     return prev > curr ? prev : curr;
+// });
+
+// console.log(output);  //10
+
+// let marks = [87, 88, 35, 92, 7, 94, 96, 63, 94, 72, 9, 2, 3, 94]     //practice Que 1
+
+// let toppers = marks.filter((val) => {
+//     return val > 90;
+// })
+// console.log(toppers);
+
+
+let n = prompt("Entre any Number");
+
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+    arr[i - 1] = i;
+}
+
+console.log(arr);
+const sum = arr.reduce((res, curr) => {
+    return res + curr;
 });
+
+console.log("Sum =", sum);
+
+const factorial = arr.reduce((res, curr) => {
+    return res * curr;
+});
+
+console.log("Factorial = ",factorial);
